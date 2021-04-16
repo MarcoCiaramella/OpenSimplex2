669d6fbf544543d51e5c76ec929f920a361b878c
MarcoCiaramella <marcociaramella87@gmail.com>
2021-04-16 14:05:30 +0200

    fixed bitmap export

--- OpenSimplex2F/test/test.c
+++ OpenSimplex2F/test/test.c
@@ -4,62 +4,121 @@
 
 
 
-#define WIDTH 100
-#define HEIGHT 100
+#define WIDTH 512
+#define HEIGHT 512
+#define BYTES_PER_PIXEL 3
+#define FILE_HEADER_SIZE 14
+#define INFO_HEADER_SIZE 40
 
 
 
-unsigned char *to_rgb_arr(int width, int height, float **vals){
-    int size = width * height * 4;
-    unsigned char *pixels = (unsigned char *) malloc(size);
-    for (int x = 0; x < width; x++){
-        for (int y = 0; y < height; y++){
-            float val = vals[x][y];
-            val = (val + 1.0) / 2.0;
-            unsigned char gray = (unsigned char) (val * 255);
-            int p = (y * width + x) * 4;
-            pixels[p + 0] = gray; //blue
-            pixels[p + 1] = gray;//green
-            pixels[p + 2] = gray;//red
-        }
-    }
-    return pixels;
+
+
+unsigned char* createBitmapInfoHeader (int height, int width){
+    static unsigned char infoHeader[] = {
+        0,0,0,0, /// header size
+        0,0,0,0, /// image width
+        0,0,0,0, /// image height
+        0,0,     /// number of color planes
+        0,0,     /// bits per pixel
+        0,0,0,0, /// compression
+        0,0,0,0, /// image size
+        0,0,0,0, /// horizontal resolution
+        0,0,0,0, /// vertical resolution
+        0,0,0,0, /// colors in color table
+        0,0,0,0, /// important color count
+    };
+
+    infoHeader[ 0] = (unsigned char)(INFO_HEADER_SIZE);
+    infoHeader[ 4] = (unsigned char)(width      );
+    infoHeader[ 5] = (unsigned char)(width >>  8);
+    infoHeader[ 6] = (unsigned char)(width >> 16);
+    infoHeader[ 7] = (unsigned char)(width >> 24);
+    infoHeader[ 8] = (unsigned char)(height      );
+    infoHeader[ 9] = (unsigned char)(height >>  8);
+    infoHeader[10] = (unsigned char)(height >> 16);
+    infoHeader[11] = (unsigned char)(height >> 24);
+    infoHeader[12] = (unsigned char)(1);
+    infoHeader[14] = (unsigned char)(BYTES_PER_PIXEL*8);
+
+    return infoHeader;
 }
 
-void save_bitmap(char *filename, int width, int height, float **vals){
-    int size = width * height * 4; //for 32-bit bitmap only
+unsigned char* createBitmapFileHeader (int height, int stride){
+    int fileSize = FILE_HEADER_SIZE + INFO_HEADER_SIZE + (stride * height);
 
-    char header[54] = { 0 };
-    strcpy(header, "BM");
-    memset(&header[2],  (int)(54 + size), 1);
-    memset(&header[10], (int)54, 1);//always 54
-    memset(&header[14], (int)40, 1);//always 40
-    memset(&header[18], (int)width, 1);
-    memset(&header[22], (int)height, 1);
-    memset(&header[26], (short)1, 1);
-    memset(&header[28], (short)32, 1);//32bit
-    memset(&header[34], (int)size, 1);//pixel size
+    static unsigned char fileHeader[] = {
+        0,0,     /// signature
+        0,0,0,0, /// image file size in bytes
+        0,0,0,0, /// reserved
+        0,0,0,0, /// start of pixel array
+    };
 
-    unsigned char *pixels = to_rgb_arr(width, height, vals);
+    fileHeader[ 0] = (unsigned char)('B');
+    fileHeader[ 1] = (unsigned char)('M');
+    fileHeader[ 2] = (unsigned char)(fileSize      );
+    fileHeader[ 3] = (unsigned char)(fileSize >>  8);
+    fileHeader[ 4] = (unsigned char)(fileSize >> 16);
+    fileHeader[ 5] = (unsigned char)(fileSize >> 24);
+    fileHeader[10] = (unsigned char)(FILE_HEADER_SIZE + INFO_HEADER_SIZE);
 
-    FILE *fout = fopen(filename, "wb");
-    fwrite(header, 1, 54, fout);
-    fwrite(pixels, 1, size, fout);
-    free(pixels);
-    fclose(fout);
+    return fileHeader;
+}
+
+void generateBitmapImage(unsigned char* image, int height, int width, char* imageFileName){
+    int widthInBytes = width * BYTES_PER_PIXEL;
+
+    unsigned char padding[3] = {0, 0, 0};
+    int paddingSize = (4 - (widthInBytes) % 4) % 4;
+
+    int stride = (widthInBytes) + paddingSize;
+
+    FILE* imageFile = fopen(imageFileName, "wb");
+
+    unsigned char* fileHeader = createBitmapFileHeader(height, stride);
+    fwrite(fileHeader, 1, FILE_HEADER_SIZE, imageFile);
+
+    unsigned char* infoHeader = createBitmapInfoHeader(height, width);
+    fwrite(infoHeader, 1, INFO_HEADER_SIZE, imageFile);
+
+    int i;
+    for (i = 0; i < height; i++) {
+        fwrite(image + (i*widthInBytes), BYTES_PER_PIXEL, width, imageFile);
+        fwrite(padding, 1, paddingSize, imageFile);
+    }
+
+    fclose(imageFile);
+}
+
+void save_bitmap(char *filename, float **vals){
+    unsigned char image[HEIGHT][WIDTH][BYTES_PER_PIXEL];
+
+    int i, j;
+    for (i = 0; i < HEIGHT; i++) {
+        for (j = 0; j < WIDTH; j++) {
+            float val = vals[i][j];
+            val = (val + 1.0) / 2.0;
+            unsigned char gray = (unsigned char) (val * 255);
+            image[i][j][2] = gray; // red
+            image[i][j][1] = gray; // green
+            image[i][j][0] = gray; // blue
+        }
+    }
+
+    generateBitmapImage((unsigned char*) image, HEIGHT, WIDTH, filename);
 }
 
 int main(){
     OpenSimplexEnv *ose = initOpenSimplex();
     OpenSimplexGradients *osg = newOpenSimplexGradients(ose, 1234);
-    float **noise = (float **) malloc(sizeof(float *) * WIDTH);
-    for (int x = 0; x < WIDTH; x++){
-        noise[x] = (float *) malloc(sizeof(float) * HEIGHT);
-        for (int y = 0; y < HEIGHT; y++){
-            noise[x][y] = noise2(ose, osg, x, y);
+    float **noise = (float **) malloc(sizeof(float *) * HEIGHT);
+    for (int y = 0; y < HEIGHT; y++){
+        noise[y] = (float *) malloc(sizeof(float) * WIDTH);
+        for (int x = 0; x < WIDTH; x++){
+            noise[y][x] = noise2(ose, osg, x, y);
         }
     }
-    save_bitmap("noise2.bmp", WIDTH, HEIGHT, noise);
+    save_bitmap("noise2.bmp", noise);
 
     for (int x = 0; x < 10; x++){
         for (int y = 0; y < 10; y++){
