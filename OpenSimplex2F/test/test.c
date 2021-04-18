ae7c1c75e91823494e0ba3df0ef79ded9961ebbd
MarcoCiaramella <marcociaramella87@gmail.com>
2021-04-19 00:49:41 +0200

    new test

--- OpenSimplex2F/test/test.c
+++ OpenSimplex2F/test/test.c
@@ -5,6 +5,13 @@
 
 
 
+#define PERIOD 64.0
+#define OFF_X 2048
+#define OFF_Y 2048	
+#define FREQ 1.0 / PERIOD
+
+
+
 int main(){
     test();
     OpenSimplexEnv *ose = initOpenSimplex();
@@ -13,18 +20,21 @@ int main(){
     for (int y = 0; y < HEIGHT; y++){
         noise[y] = (double *) malloc(sizeof(double) * WIDTH);
         for (int x = 0; x < WIDTH; x++){
-            noise[y][x] = noise2(ose, osg, x, y);
+            noise[y][x] = noise2(ose, osg, (x + OFF_X) * FREQ, (y + OFF_Y) * FREQ);
         }
     }
     save_bitmap("noise2.bmp", noise);
 
-    for (int x = 0; x < 10; x++){
-        for (int y = 0; y < 10; y++){
-            for (int z = 0; z < 10; z++){
-                double val = noise3_Classic(ose, osg, x, y, z);
-            }
+    free(noise);
+
+    noise = (double **) malloc(sizeof(double *) * HEIGHT);
+    for (int y = 0; y < HEIGHT; y++){
+        noise[y] = (double *) malloc(sizeof(double) * WIDTH);
+        for (int x = 0; x < WIDTH; x++){
+            noise[y][x] = noise3_Classic(ose, osg, (x + OFF_X) * FREQ, 0.0, (y + OFF_Y) * FREQ);
         }
     }
+    save_bitmap("noise3.bmp", noise);
 
     return 0;
 }
